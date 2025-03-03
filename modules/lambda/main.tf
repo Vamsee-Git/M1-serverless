3edaaedbfb1692ea50fd8dfb06717811f28f1bed
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-03-03 07:39:45 +0530

    Update main.tf

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -65,11 +65,3 @@ resource "aws_lambda_function" "get_data" {
     }
   }
 }
-
-output "add_data_lambda_name" {
-  value = aws_lambda_function.add_data.function_name
-}
-
-output "get_data_lambda_name" {
-  value = aws_lambda_function.get_data.function_name
-}
